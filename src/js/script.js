0588b600b95cceee0174a209206d346078769524
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2025-05-08 19:38:50 -0300

    En fase de teste

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-const titulo = document.getElementById("titulo-animado");
-let posicao = 0;
-let direcao = 1;
-
-setInterval(() => {
-    if (posicao > 30 || posicao < 0) direcao *= -3;
-    posicao += direcao;
-    titulo.style.transform = `translateX(${posicao}px)`;
-}, 30);
